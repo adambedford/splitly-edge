3b25c3808a9d405b0a279e3cd34d801d93c8fe8b
Adam Bedford <adam.bedford@live.com>
2023-08-22 23:21:27 -0700

    Load latest version of JS tracker

--- netlify/edge-functions/tracker.js
+++ netlify/edge-functions/tracker.js
@@ -1,6 +1,6 @@
 export default async function(request, context) {
   const content = await fetch(
-    'https://unpkg.com/splitly-tracker@0.0.1/dist/sp-tr.umd.js'
+    'https://unpkg.com/splitly-tracker/dist/sp-tr.umd.js'
   );
 
   return new Response(content.body, content);
